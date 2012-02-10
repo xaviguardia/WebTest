91913a01dd6209269e64e569b93c86bb1981edfe
xaviguardia <xavi.guardia@gmail.com>
2012-02-10 20:09:46 +0100

    Corregit problema

--- Program.cs
+++ Program.cs
@@ -9,8 +9,8 @@ namespace MiPrueba
     {
         static void Main(string[] args)
         {
-
-            Black BackGround !
+            // This was a comment
+            // Black BackGround !
             //
 
             // blbvlblbls
